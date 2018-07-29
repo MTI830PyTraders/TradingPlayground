0017c4c84ec6067a6b3063f894c11ebf62edc9b8
Benoit Paquet <benoxoft@gmail.com>
2018-07-29 03:05:18 -0400

    fix eps usage

--- data_manipulation.py
+++ data_manipulation.py
@@ -29,18 +29,20 @@ def prepare_training_data(train_data: pd.DataFrame, extra_input_list_str=[]) -> 
     # close_p_scaler = preprocessing.MinMaxScaler()
     # close_p = (close[1:] - close[:-1]) / close[1:]
     # close_p = close_p_scaler.fit_transform(close_p)
-    peg_ratio_scaler = preprocessing.MinMaxScaler()
-    peg_ratio = (train_data['close'] / train_data['eps'])
-    peg_ratio = np.array(peg_ratio).reshape((len(peg_ratio), 1))
-    peg_ratio = peg_ratio_scaler.fit_transform(peg_ratio)
 
-    extra_input_list = [close[1:], peg_ratio[1:]]
-    extra_scaler_list = []
+    extra_input_list = [close[1:], ]
+
+    if 'eps' in train_data.keys():
+        peg_ratio_scaler = preprocessing.MinMaxScaler()
+        peg_ratio = (train_data['close'] / train_data['eps'])
+        peg_ratio = np.array(peg_ratio).reshape((len(peg_ratio), 1))
+        peg_ratio = peg_ratio_scaler.fit_transform(peg_ratio)
+        extra_input_list.append(peg_ratio[1:])
+
     if extra_input_list_str:
         for input in extra_input_list_str:
             extra_input, extra_scaler = generate_input_data(train_data, input)
             extra_input_list = extra_input_list + [extra_input[1:]]
-            extra_input_scaler = extra_scaler_list + [extra_scaler]
 
     data_to_concat = tuple(extra_input_list)
     data = np.concatenate(data_to_concat, axis=1)
