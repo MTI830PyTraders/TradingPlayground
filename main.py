a0783e9dda5ea491009394b1af0f64f9bd524434
Michaël Faille <michael@faille.io>
2018-07-28 22:38:37 -0400

    Refactor lstm multi to get config profile. Rename it to main.py
    Some others stuffs done on lstm code :
    - dont show up pyplot diags on ui

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
+import matplotlib
+matplotlib.use('Agg')
 import data_manipulation
 import lstm
 import numpy as np
 import os
 import xarray as xr
+import keras
 import math
 from keras.optimizers import Adam
+
 import matplotlib.pyplot as plt
+plt.ioff()
+from keras.callbacks import CSVLogger
+
+import toml
+import os
+
+profile = os.environ.get('LSTM_PROFILE', 'default')
+all_config = toml.load('lstm.config.toml')
+# ipdb.set_trace()
+config = all_config[profile]
 
 # Parameters of what data to select
-BEGINNING_DATE = '2013-03-31'
-ENDING_DATE = '2018-06-31'
-TICKER = 'MSFT'
+BEGINNING_DATE = config['BEGINNING_DATE']
+ENDING_DATE = config['ENDING_DATE']
 
 # These parameters will tweak the model
-BATCH_SIZE = 90
+BATCH_SIZE = config['BATCH_SIZE']
 LOSS = 'mae'
-N_HIDDEN = 1000
-NUM_EPOCHS = 100
-SAVE_EVERY = 10
-NUM_TIMESTEPS = 180
-OPTIMIZER = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-TIMESTEPS_AHEAD = 90
+N_HIDDEN = config['N_HIDDEN']
+NUM_EPOCHS = config['NUM_EPOCHS']
+SAVE_EVERY = config['SAVE_EVERY']
+NUM_TIMESTEPS = config['NUM_TIMESTEPS']
+LEARNING_RATE = config['LEARNING_RATE']
+OPTIMIZER = Adam(lr=LEARNING_RATE, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+TIMESTEPS_AHEAD = config['TIMESTEPS_AHEAD']
 VERBOSE = 0
+CONFIG_ID = profile
+
+if not os.path.exists('RESULTS'):
+    os.mkdir('RESULTS')
+if not os.path.exists(f'RESULTS/{CONFIG_ID}'):
+    os.mkdir(f'RESULTS/{CONFIG_ID}')
+csv_logger = CSVLogger(f'RESULTS/{CONFIG_ID}/keras.log.csv', append=True, separator=';')
+import logging
+# fh = logging.FileHandler('spam.log')
+# os.mknod("RESULTS" + str(CONFIG_ID) + "logfile.txt")
+logging.basicConfig(filename=f'RESULTS/{CONFIG_ID}/logfile.txt', filemode='w')
+stderrLogger = logging.StreamHandler()
+stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+logging.getLogger().addHandler(stderrLogger)
+
 
 # percentage of the data that will be used to train the model.
 TRAIN_TEST_RATIO = 0.8
@@ -32,47 +62,72 @@ np.random.seed(SEED)
 
 # actual code is done here. It is not wrapped in a function so that variables can be checked in a python console.
 
-final_xr = xr.open_dataset("final_xr.nc", chunks=30)
-ds = final_xr.sel(ticker=TICKER)
-train_data = xr.Dataset({'Sentiment': ds.sentiment, 'close': ds['close'], 'fcf': ds.fcf}).to_dataframe().interpolate(limit_direction='both')
 
-data, scaler = data_manipulation.prepare_training_data(train_data)
+base_input = ['close']
+extra_input = config['extra_input']
+input_list = base_input + extra_input
 
-if not os.path.exists('plots'):
-    os.mkdir('plots')
+final_xr = xr.open_dataset("final_xr.nc", chunks=30)[input_list]
 
-x, y = lstm.process_data_for_lstm(data, NUM_TIMESTEPS, TIMESTEPS_AHEAD)
-xtrain, xtest, ytrain, ytest = lstm.divide_data_into_train_test(x, y, TRAIN_TEST_RATIO, BATCH_SIZE)
-#model = load_trained_model()
-model = lstm.create_model(data.shape[1], N_HIDDEN, NUM_TIMESTEPS, BATCH_SIZE, OPTIMIZER, LOSS)
 
-for i in range(NUM_EPOCHS // SAVE_EVERY):
-    lstm.train_model(model, SAVE_EVERY, BATCH_SIZE, xtrain, ytrain, xtest, ytest)
-    if not os.path.exists('models'):
-        os.mkdir('models')
-    model.save('models/lstm.h5')
+model = None
+# tickers = final_xr.get('ticker').values
+tickers = config['tickers']
 
-    prediction = lstm.try_prediction(xtest, model, BATCH_SIZE)
-    prediction = data_manipulation.scale_back_to_normal(prediction, scaler)
-    test_data = data_manipulation.scale_back_to_normal(ytest[BATCH_SIZE], scaler)
-    lstm.show_prediction(prediction, test_data, f'plots/{TICKER}_{i}.png')
 
-    plt.close()
-    plt.plot(lstm.history.losses)
-    plt.plot(lstm.history.val_losses)
-    plt.title('model train vs validation loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper right')
-    plt.show()
-    plt.close()
-    plt.plot(lstm.history.mapes)
-    plt.title('mape')
-    plt.ylabel('mape')
-    plt.xlabel('epoch')
-    plt.legend(['mape'], loc='upper right')
-    plt.show()
+totalEpoch = 1
+history = lstm.LossHistory()
+
+for ticker in tickers:
+    ds = final_xr.sel(ticker=ticker)
+    train_data = ds.to_dataframe().interpolate(limit_direction='both')
+    data, scaler = data_manipulation.prepare_training_data(train_data, extra_input_list_str=extra_input)
+
+    if not model:
+        model = lstm.create_model(data.shape[1], N_HIDDEN, NUM_TIMESTEPS,
+                                  BATCH_SIZE, OPTIMIZER, LOSS)
+
+    x, y = lstm.process_data_for_lstm(data, NUM_TIMESTEPS, TIMESTEPS_AHEAD)
+    xtrain, xtest, ytrain, ytest = lstm.divide_data_into_train_test(x, y, TRAIN_TEST_RATIO, BATCH_SIZE)
+    for i in range(NUM_EPOCHS // SAVE_EVERY):
+        print(f"epoch number: {totalEpoch}")
+        lstm.train_model(model, SAVE_EVERY, BATCH_SIZE, xtrain, ytrain,
+                         xtest, ytest, extraCallback=[csv_logger, history])
+        if not os.path.exists('models'):
+            os.mkdir('models')
+        model.save('models/lstm.h5')
+
+        prediction = lstm.try_prediction(xtest, model, BATCH_SIZE)
+        prediction = data_manipulation.scale_back_to_normal(prediction, scaler)
+        test_data = data_manipulation.scale_back_to_normal(ytest[BATCH_SIZE], scaler)
+        if not os.path.exists(f'RESULTS/{CONFIG_ID}/predict'):
+            os.mkdir(f'RESULTS/{CONFIG_ID}/predict')
+        lstm.show_prediction(prediction, test_data, ticker,
+                             file_name=f"RESULTS/{CONFIG_ID}/predict/predictEpoch{totalEpoch}.png")
+
+        plt.close("all")
+        history
+        plt.plot(history.losses)
+        plt.plot(history.val_losses)
+        plt.title("model train vs validation loss - Ticker: " + ticker)
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper right')
+        plt.savefig(f'RESULTS/{CONFIG_ID}/loss.png')
+        plt.show()
+        plt.close("all")
+
+        plt.figure()
+        plt.plot(history.mapes)
+        plt.title("mape - ticker: " + ticker)
+        plt.ylabel('mape')
+        plt.xlabel('epoch')
+        plt.legend(['mape'], loc='upper right')
+        plt.savefig(f'RESULTS/{CONFIG_ID}/mape.png')
+        plt.show()
+        plt.close("all")
+        totalEpoch = totalEpoch + 1
 
 score, _, _ = model.evaluate(xtest, ytest, batch_size=BATCH_SIZE)
 rmse = math.sqrt(score)
-print("\nMSE: {:.3f}, RMSE: {:.3f}".format(score, rmse))
+logging.info(r"\nMSE: {:.3f}, RMSE: {:.3f}".format(score, rmse))
